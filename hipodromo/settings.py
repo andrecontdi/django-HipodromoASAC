e389c061d1a86d1ee1d9db66a4d0ec7eacd698e5
Atahualpa Silva F <atahualpasilva@gmail.com>
2018-07-19 23:11:07 -0400

    Agregando clase y subclases de persona.
    Subclases agregadas: Caballerizo, Entrenador, Invitado, Jinete,
    Propietario, Taquillero, Veterinario.

--- hipodromo/settings.py
+++ hipodromo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from hipodromo.dev_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,4 +125,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-from hipodromo.dev_settings import *
+AUTH_USER_MODEL = 'users.Persona'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,72 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+from decimal import Decimal
+
+
 # Create your models here.
+class Persona(AbstractUser):
+    cedula = models.PositiveIntegerField(unique=True)
+    primer_nombre = models.CharField(max_length=25)
+    segundo_nombre = models.CharField(max_length=25)
+    primer_apellido = models.CharField(max_length=25)
+    segundo_apellido = models.CharField(max_length=25)
+    fecha_nacimiento = models.DateField()
+
+
+class Caballerizo(Persona):
+    pass
+
+
+class Entrenador(Persona):
+    pass
+
+
+class Invitado(Persona):
+    ARTISTA = 'A'
+    DEPORTISTA = 'D'
+    LIDER_OPINION = 'L'
+    OPCIONES_TIPO_INVITADO = (
+        (ARTISTA, 'Artista'),
+        (DEPORTISTA, 'Deportista'),
+        (LIDER_OPINION, 'Líder de opinión'),
+    )
+    tipo_invitado = models.CharField(
+        max_length=1,
+        choices=OPCIONES_TIPO_INVITADO,
+        default=ARTISTA,
+    )
+
+
+class Jinete(Persona):
+    APRENDIZ = 'A'
+    PROFESIONAL = 'P'
+    OPCIONES_EXPERIENCIA = (
+        (APRENDIZ, 'Aprendiz'),
+        (PROFESIONAL, 'Profesional'),
+    )
+    experiencia = models.CharField(
+        max_length=1,
+        choices=OPCIONES_EXPERIENCIA,
+    )
+    altura = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(Decimal('0.80')),
+            MaxValueValidator(Decimal('2.50'))
+        ]
+    )
+
+
+class Propietario(Persona):
+    pass
+
+
+class Taquillero(Persona):
+    pass
+
+
+class Veterinario(Persona):
+    pass
